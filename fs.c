4ca1c013d9bc922ff027a1a4fe1bb4fc3198509f
moipalamoi <moimoipalamoi@gmail.com>
2017-12-06 19:26:34 +0800

    First commit

---
+++ fs.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+int main(int argc, char* argv[]){
+	
+
+	mvFunc("foo", "foonew");
+
+	return 0;
+}
---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef header_h
+#define header_h
+
+#include "ls.h"
+#include "rm.h"
+#include "mv.h"
+#include "touch.h"
+#include "cat.h"
+
+#endif
