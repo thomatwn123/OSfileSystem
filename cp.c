d9612a075da90736053e89d277fe74c1009bd6d9
Emethteme <Emethteme@gmail.com>
2017-12-06 11:33:07 +0800

    Add files via upload

---
+++ cp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])	{
+	
+	if (argc != 3) {
+		printf("Invalid argument.\n");
+		return 1;
+	}
+
+	FILE *fr = fopen(argv[1], "rb");
+	FILE *fw = fopen(argv[2], "wb");
+
+	if (!fr) {
+		printf("Fail to READ file.\n");
+		return 1;
+	}
+
+	if (!fw) {
+		printf("Fail to WRITE file.\n");
+		return 1;
+	}
+
+	char ch;
+
+	while (!feof(fr)) {
+		fread(&ch, sizeof(char), 1, fr);
+
+		if (!feof(fr)) {
+			fwrite(&ch, sizeof(char), 1, fw);
+		}
+
+		printf("%s\n", &ch);
+	}
+
+	fclose(fr);
+	fclose(fw);
+	
+	return 0;
+}
+
